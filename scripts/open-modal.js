53ebf2a95e85417275be4b162b5cae776d2128cf
Joshua <teoxonjoshua13@gmail.com>
2024-03-26 11:42:02 +0800

    delete console log

--- scripts/open-modal.js
+++ scripts/open-modal.js
@@ -5,8 +5,6 @@ const readMoreButtonElements = document.querySelectorAll(
   "#projects .project-grid button"
 );
 
-console.log(readMoreButtonElements);
-
 export function openModal(event) {
   modalElement.showModal();
   const button = event.target;
