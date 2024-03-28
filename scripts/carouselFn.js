55c7b7d3b2dfda1867af881119c29f118b596433
Joshua <teoxonjoshua13@gmail.com>
2024-03-28 13:40:40 +0800

    cleaning my code

--- scripts/carouselFn.js
+++ scripts/carouselFn.js
@@ -4,26 +4,28 @@ export function carouselFn(carouselImages, modalElement) {
   const buttonRightElement = document.getElementById("button-right");
 
   let currentIndex = 0;
-
+  // fn for current slide
   function showSlide(index) {
     for (const carouselImage of carouselImages) {
       carouselImage.style.display = "none";
     }
-
     carouselImages[index].style.display = "block";
   }
 
+  // prev slide
   function previousSlide() {
     currentIndex = (currentIndex + 1) % carouselImages.length;
     showSlide(currentIndex);
   }
 
+  // next slide
   function nextSlide() {
     currentIndex =
       (currentIndex - 1 + carouselImages.length) % carouselImages.length;
     showSlide(currentIndex);
   }
 
+  // running for current slide
   showSlide(currentIndex);
 
   buttonLeftElement.addEventListener("click", nextSlide);
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
 import { toggleAside } from "./open-aside.js";
 import { openModal } from "./open-modal.js";
 import { carouselFn } from "./carouselFn.js";
+import { sendingEmail } from "./form.js";
