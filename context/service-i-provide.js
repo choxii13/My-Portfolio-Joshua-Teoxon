770fff66306ae8c6657d08befab6153069fb89a4
Joshua <teoxonjoshua13@gmail.com>
2024-05-07 10:47:52 +0800

    removing design of the border in works images, cleaning the design and adding design

--- context/service-i-provide.js
+++ context/service-i-provide.js
@@ -2,32 +2,45 @@ export const services = [
   {
     icon: "bi-code-slash",
     title: "Front-end Development",
-    para: "Create interactive websites and developing new features. Building reusable code and components that interact with users",
+    para: `Create interactive websites and developing 
+    new features. Building reusable code 
+    and components that interact with users`,
   },
   {
     icon: "bi-brush",
     title: "Web Design",
-    para: "Create visual elements and layouts with minimal code. Translating design into functional website. Clear and concise web design.",
+    para: `Create visual elements and layouts 
+    with minimal code. Translating design into
+    functional website. Clear and concise web design.`,
   },
   {
     icon: "bi-palette",
     title: "UI/UX Design",
-    para: "Making a product that is enjoyable, accessible and pleasing to the eye of user. Improving the user interaction with products.",
+    para: `Making a product that is enjoyable, 
+    accessible and pleasing to the eye of user. 
+    Improving the user interaction with products.`,
   },
   {
     icon: "bi-gear",
     title: "Backend Development",
-    para: "Creating server-side-application. Develop each element of the server-side maitaining and debugging codes in website. api, databases, and logic.",
+    para: `Creating server-side-application. 
+    Develop each element of the server-side 
+    maitaining and debugging codes in website. 
+    api, databases, and logic.`,
   },
   {
     icon: "bi-window",
     title: "Web Development",
-    para: "Full functionality of the web development like shoppee, lazada etc. Interactive website with backend. Ensuring the website to function properly.",
+    para: `Full functionality of the web development
+     like shoppee, lazada etc. Interactive website 
+     with backend. Ensuring the website to function properly.`,
   },
   {
     icon: "bi-shield-exclamation",
     title: "Security",
-    para: "Creating security for website used by many companies hashed, csrf attacks, injections, authorizations, and authentication.",
+    para: `Creating security for website used 
+    by many companies such as hashed, csrf attacks, injections, 
+    authorizations, and authentication.`,
   },
 ];
 
@@ -40,7 +53,7 @@ for (let i = 0; i < services.length; i++) {
     </div>
     <h4> ${services[i].title} </h4>
     <p>
-    ${services[i].para}.
+    ${services[i].para}
     </p>
   </div>`;
 }
